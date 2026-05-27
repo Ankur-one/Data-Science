7fb9d9ed5e82e59ccdac20425420eb12efbe7263
Ankur Kumar <2005ankurkumar@gmail.com>
2026-05-27 17:58:54 +0530

    Add files via upload

---
+++ Data Science/Python/Variable & Data type/Hello.py
@@ -0,0 +1,6 @@
+print("Hello World")
+
+Instructor = 'Rishabh Mishra'
+print("Python by", Instructor)
+
+print(5+5)
---
+++ Data Science/Python/Variable & Data type/Input.py
@@ -0,0 +1,36 @@
+# name = input("Enter your name : ")
+# print("Hello "+ name +"!")
+
+# x = input("Enter 1st number : ")
+# y = input("Enter 2nd number : ")
+# print(f"sum of {x} & {y} is {int(x) + int(y)}")
+
+
+
+# Name = input("Student Name : ")
+# sub1 = input("Math Marks : ")
+# sub2 = input("Science marks : ")
+# sub3 = input("English Marks : ")
+# Percentage = ((float(sub1) + float(sub2) + float(sub3))/300)*100
+# print(f"Student Name : {Name} + and its % is {float(Percentage)}%")
+
+
+# Dictinory
+
+# Student = {
+#     "Name" : "Ankur",
+#     "Age" : 20,
+#     "Height" : 6,
+#     "Qualification" : "B.tech"
+# }
+# print(Student)
+
+
+
+Student = {}
+
+Student['Name'] = input("Enter your name : ")
+Student['Agr'] = int(input("Enter your age : "))
+Student['Height'] = float(input("Enter your height : "))
+Student['Qalification'] = input("Enter your Qualification : ")
+print(Student)
