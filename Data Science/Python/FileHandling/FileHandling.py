bb96d59e792576025a73898ccfd4697edb708f0f
Ankur Kumar <2005ankurkumar@gmail.com>
2026-06-09 20:50:34 +0530

    Add files via upload

---
+++ Data Science/Python/FileHandling/FileHandling.py
@@ -0,0 +1,24 @@
+# Open file
+# file = open('Example.txt', 'r')
+
+# # read file
+# file = open('Example.txt', 'r')
+# content = file.read()
+# print(content)
+# file.close
+
+# file = open('Example.txt', 'r')
+# content = file.readlines()
+# print(content)
+# file.close
+
+
+# # Write file
+# file = open('Example.txt', 'w')
+# file.write("Ankur")
+# file.close
+
+# Append mode
+file = open('Example.txt', 'a')
+file.write(" Add kro")
+file.close
