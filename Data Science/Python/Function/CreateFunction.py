28f493063e0a558a13bf0e0eacbf882056695a57
Ankur Kumar <2005ankurkumar@gmail.com>
2026-06-09 20:48:38 +0530

    Add files via upload

---
+++ Data Science/Python/Function/CreateFunction.py
@@ -0,0 +1,11 @@
+     # Function Without Parametres
+# def function():
+#     print("Welcome! Ankur Singh")
+# function()   
+
+
+      # Function With Parametres
+def function(a, b):
+    result = a+b
+    print(f"The sum is {result}")
+function(8, 5)    
